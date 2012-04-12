9b5f3207432327bd10d5090d00867d69b0871e77
Sean Fisk <sean@seanfisk.com>
2012-04-12 12:30:59 -0400

    Make the labels on the board clickable.

--- buzzword_bingo_client/gui/main_window.py
+++ buzzword_bingo_client/gui/main_window.py
@@ -33,6 +33,58 @@ from about import AboutDialog
 from buzzword_bingo_client import metadata
 
 SIZE = 5
+COLOR_UNCOVERED = QtGui.QColor(244, 244, 244)
+COLOR_COVERED = QtCore.Qt.lightGray
+
+# set the background and frame color for the labels
+LABEL_PALETTE = QtGui.QPalette()
+LABEL_PALETTE.setColor(QtGui.QPalette.WindowText, QtCore.Qt.black)
+LABEL_PALETTE.setColor(QtGui.QPalette.Window,
+                 COLOR_UNCOVERED)
+LABEL_PALETTE.setColor(QtGui.QPalette.Light, QtCore.Qt.black)
+LABEL_PALETTE.setColor(QtGui.QPalette.Dark, QtCore.Qt.black)
+
+class BoardLabel(QtGui.QLabel):
+    """A square for the bingo board."""
+    
+    clicked = QtCore.Signal(int, int)
+    
+    def __init__(self, row, col, text, parent=None, f=0):
+        """Construct a board label.
+        
+        :param text: string to show
+        :type text: :class:`str`
+        :param coords: coordinates of this label
+        :type coords: :class:`tuple` of :class:`int`
+        :param parent: parent in the Qt hierarchy
+        :type parent: :class:`QtGui.QWidget`
+        :param f: window flags
+        :type f: :class:`QtCore.Qt.WindowFlags`
+        """
+        super(BoardLabel, self).__init__(text, parent, f)
+        # the following line is nesscary to paint the background of the
+        # widget
+        self.setAutoFillBackground(True)
+        self.setPalette(LABEL_PALETTE)
+        self.setFrameShape(QtGui.QFrame.Box)
+        self.setFrameShadow(QtGui.QFrame.Raised)
+        self.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+        self.row = row
+        self.col = col
+
+    def mousePressEvent(self, event):
+        """Handle mouse press events.
+
+        clicked.emt
+        
+        :param event: the mouse press event
+        :type event: :class:`QtGui.QMouseEvent`
+        """
+        palette = self.palette()
+        palette.setColor(QtGui.QPalette.Window, COLOR_COVERED)
+        self.setPalette(palette)
+        self.clicked.emit(self.row, self.col)
+
 
 class MainWindow(QtGui.QMainWindow):
     """Main application window."""
@@ -53,14 +105,6 @@ class MainWindow(QtGui.QMainWindow):
 
         self.setCentralWidget(QtGui.QWidget(self))
 
-        # set the background and frame color for the labels
-        palette = QtGui.QPalette()
-        palette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.black)
-        palette.setColor(QtGui.QPalette.Window,
-                         QtGui.QColor(244, 244, 244))
-        palette.setColor(QtGui.QPalette.Light, QtCore.Qt.black)
-        palette.setColor(QtGui.QPalette.Dark, QtCore.Qt.black)
-
         self.layout = QtGui.QGridLayout(self.centralWidget())
         self.layout.setSpacing(0)
         self.layout.setContentsMargins(QtCore.QMargins())
@@ -72,19 +116,16 @@ class MainWindow(QtGui.QMainWindow):
                     text = 'FREE SPACE'
                 else:
                     text = 'Cloud Computing'
-                label = QtGui.QLabel(text, self.centralWidget())
-                # the following line is nesscary to paint the background of the
-                # widget
-                label.setAutoFillBackground(True)
-                label.setPalette(palette)
-                label.setFrameShape(QtGui.QFrame.Box)
-                label.setFrameShadow(QtGui.QFrame.Raised)
-                label.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
-
+                label = BoardLabel(row, col, text, self.centralWidget())
+                label.clicked.connect(self.label_clicked)
                 self.layout.addWidget(label, row, col)
         
     @QtCore.Slot()
     def about(self):
         """Create and show the about dialog."""
         AboutDialog(self).exec_()
+
+    @QtCore.Slot(int, int)
+    def label_clicked(self, row, col):
+        print row, col
 
